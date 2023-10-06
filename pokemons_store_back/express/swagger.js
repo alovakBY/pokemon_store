16b05fc0f6a75c44239678b7fd8d1f955a7344cc
Artem_Loseu <mailto:alovakby@gmail.com>
2023-10-06 11:02:37 +0300

    refactor: change swagger UI url, remove console logs

--- pokemons_store_back/express/swagger.js
+++ pokemons_store_back/express/swagger.js
@@ -30,9 +30,5 @@ const options = {
 const specs = swaggerJsdoc(options);
 
 module.exports = (app) => {
-  app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(specs, { explorer: true }),
-  );
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
 };
